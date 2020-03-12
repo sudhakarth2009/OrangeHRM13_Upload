4d94c32c95df8f51b8e32d385ec978bcf090f678
LENOVO <LENOVO@DESKTOP-07LUTLC>
2020-03-12 09:11:49 +0530

    push command on march 13

---
+++ pinky/src/main/java/pinky/pinky/App.java
@@ -0,0 +1,13 @@
+package pinky.pinky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
